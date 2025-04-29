93392a88f17e58acf4c28594a5a093757a7c8215
원석규 <angenehms@naver.com>
2025-04-29 09:48:18 +0900

    [S12P31A607-87] ai/chore: modify import route of api router #done

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routers import api_router
+from api.routers import api_router
 import uvicorn
 
 app = FastAPI()
