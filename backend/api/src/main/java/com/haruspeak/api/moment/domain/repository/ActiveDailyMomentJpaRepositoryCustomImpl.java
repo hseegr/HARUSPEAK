59f72a7883858386ab81431a0f46d0df9105f151
조강민 <jkm96529@naver.com>
2025-04-29 17:32:06 +0900

    [S12P31A607-98] be/feat: Define MomentList #done

--- backend/api/src/main/java/com/haruspeak/api/moment/domain/repository/ActiveDailyMomentJpaRepositoryCustomImpl.java
+++ backend/api/src/main/java/com/haruspeak/api/moment/domain/repository/ActiveDailyMomentJpaRepositoryCustomImpl.java
@@ -1,13 +1,19 @@
 package com.haruspeak.api.moment.domain.repository;
 
+import com.haruspeak.api.moment.domain.ActiveDailyMoment;
 import com.haruspeak.api.moment.domain.QActiveDailyMoment;
 import com.haruspeak.api.moment.dto.MomentDetailRaw;
+import com.haruspeak.api.moment.dto.request.MomentListRequest;
 import com.haruspeak.api.moment.dto.response.MomentDetailResponse;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,6 +22,11 @@ public class ActiveDailyMomentJpaRepositoryCustomImpl implements ActiveDailyMome
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 순간 일기 상세보기
+     * @param momentId
+     * @return
+     */
     @Override
     public Optional<MomentDetailRaw> getMomentDetailRaw(Integer momentId) {
         QActiveDailyMoment moment = QActiveDailyMoment.activeDailyMoment;
@@ -33,5 +44,44 @@ public class ActiveDailyMomentJpaRepositoryCustomImpl implements ActiveDailyMome
                 .from(moment)
                 .where(moment.momentId.eq(momentId))
                 .fetchOne());
+    }
+
+    /**
+     * 순간 일기 리스트 불러오기
+     * @param userId
+     * @param request
+     * @return
+     */
+    @Override
+    public List<ActiveDailyMoment> getMomentList(Integer userId, MomentListRequest request) {
+        QActiveDailyMoment moment = QActiveDailyMoment.activeDailyMoment;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(moment.userId.eq(userId));
+
+        if (request.before() != null) {
+            LocalDateTime beforeDateTime = request.before();
+            builder.and(moment.momentTime.lt(beforeDateTime));
+        }
+
+        if (request.startDate() != null && request.endDate() != null) {
+            builder.and(moment.momentTime.between(
+                    request.startDate().atStartOfDay(),
+                    request.endDate().atTime(23, 59, 59)
+            ));
+        }
+
+        if (request.userTags() != null && !request.userTags().isEmpty()) {
+            for (String userTag : request.userTags()) {
+                builder.and(moment.tags.like("%" + userTag + "%"));
+            }
+        }
+
+        return queryFactory
+                .selectFrom(moment)
+                .where(builder)
+                .orderBy(moment.momentTime.desc())
+                .limit(request.limit() != null ? request.limit() : 30)
+                .fetch();
     }
 }
