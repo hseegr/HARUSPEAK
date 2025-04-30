1b5e390794b8a0a13bc7f78c9ab892d724907e45
김희수 <hseegr@naver.com>
2025-04-30 17:25:30 +0900

    [S12P31A607-57] fe/feat: create loginpage #in-progress

---
+++ frontend/src/apis/accountApi.ts
@@ -0,0 +1,19 @@
+import { axiosInstance } from './core';
+
+// 구글 로그인
+// 리다이렉트 -> 브라우저가 직접 이동해야 함
+export const googleLogin = async () => {
+  window.location.href = `${import.meta.env.VITE_API_DOMAIN}/auth/google/login`;
+};
+
+// 로그아웃
+export const userLogout = async () => {
+  const response = await axiosInstance.post('/auth/logout');
+  return response.data;
+};
+
+// 유저 정보 받아오기
+export const userInfo = async () => {
+  const response = await axiosInstance.get('/user/me');
+  return response.data;
+};
---
+++ frontend/src/pages/login/LoginPage.tsx
@@ -0,0 +1,34 @@
+import { googleLogin } from '@/apis/accountApi';
+import googleLogo from '@/assets/images/googlelogin.png';
+
+const LoginPage = () => {
+  const handleGoogleLogin = () => {
+    googleLogin();
+  };
+
+  return (
+    // 기존 baselayout 스타일 유지하기 위해 동일하게 구현 (현재 로그인 페이지는 baselayout 사용하지 않음)
+    <div className='flex flex-col items-center bg-gray-100'>
+      <div className='min-h-lvh w-full max-w-96 bg-white'>
+        <div className='flex min-h-[100vh] w-full flex-col items-center justify-center gap-20'>
+          <div className='flex flex-col items-center gap-2'>
+            <div className='text-2xl font-bold'>하루스픽</div>
+            <div className='text-sm font-medium'>
+              간편하게 로그인하고 Haru's Peak 시작하기
+            </div>
+          </div>
+          <div className='flex w-1/2 justify-center'>
+            <button
+              onClick={handleGoogleLogin}
+              className='flex items-center justify-center'
+            >
+              <img src={googleLogo} alt='googleLogo' className='w-64' />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
