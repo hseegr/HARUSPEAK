d14be6547aa2a8d0bc63e357fbc18b5424d59d6c
김희수 <hseegr@naver.com>
2025-04-30 17:47:33 +0900

    [S12P31A607-57] fe/feat: fix name #in-progress

--- frontend/src/apis/accountApi.ts
+++ frontend/src/apis/accountApi.ts
@@ -1,9 +1,11 @@
 import { axiosInstance } from './core';
 
+const DOMAIN = import.meta.env.VITE_API_DOMAIN;
+
 // 구글 로그인
 // 리다이렉트 -> 브라우저가 직접 이동해야 함
 export const googleLogin = async () => {
-  window.location.href = `${import.meta.env.VITE_API_DOMAIN}/auth/google/login`;
+  window.location.href = `${DOMAIN}/auth/google/login`;
 };
 
 // 로그아웃
