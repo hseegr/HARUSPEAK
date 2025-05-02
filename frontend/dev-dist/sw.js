08411b7410f072b0737ad16a1a7011d53a771f48
minjee kim <hbmj1017@naver.com>
2025-05-02 14:52:38 +0900

    [S12P31A607-131] fe/fix: refactor timeUtils and change data set of api res

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4toa06bq66o"
+    "revision": "0.4phhfl7m2mg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
