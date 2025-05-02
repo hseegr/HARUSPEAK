30a479abf8454befa6b49d5042b2011fa0db4d29
Zoe <ddu_buk_e@naver.com>
2025-05-02 13:44:05 +0900

    [S12P31A607-55] fe/feat: modify my library, diary

--- frontend/src/pages/library/index.tsx
+++ frontend/src/pages/library/index.tsx
@@ -1,17 +1,33 @@
+import { useGetLibrary } from '@/hooks/useLibraryQuery';
+import { Diary } from '@/hooks/useLibraryQuery';
+
 import DeleteBtn from './components/DeleteBtn';
 import DiaryFrame from './components/DiaryFrame';
 import Filter from './components/FilterBadge';
 
 // 내 서재에서 하루일기 모아보기
 const Library = () => {
+  const { data } = useGetLibrary();
+
   return (
     <div>
       <div>
         <Filter />
         <DeleteBtn />
-        <DiaryFrame />
+        {data?.data.map((diary: Diary) => (
+          <DiaryFrame
+            key={diary.summaryId}
+            summaryId={diary.summaryId}
+            diaryDate={diary.diaryDate}
+            imageUrl={diary.imageUrl}
+            title={diary.title}
+            // content={diary.content}
+            momentCount={diary.momentCount}
+          />
+        ))}
       </div>
     </div>
   );
 };
+
 export default Library;
