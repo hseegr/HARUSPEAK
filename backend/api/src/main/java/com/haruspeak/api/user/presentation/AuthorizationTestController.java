f70c24074814aa2bf48fb2a3933c7050cbc30b59
이지희 <chaehee13@naver.com>
2025-05-05 04:04:39 +0900

    [S12P31A607-149] be/document: add swagger hidden

--- backend/api/src/main/java/com/haruspeak/api/user/presentation/AuthorizationTestController.java
+++ backend/api/src/main/java/com/haruspeak/api/user/presentation/AuthorizationTestController.java
@@ -1,11 +1,13 @@
 package com.haruspeak.api.user.presentation;
 
 import com.haruspeak.api.common.security.AuthenticatedUser;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController()
 @RequestMapping("/test")
 public class AuthorizationTestController {
