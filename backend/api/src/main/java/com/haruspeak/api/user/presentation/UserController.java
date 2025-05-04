6eb28e5dd13c54c6778480d506ff954b11e02fd8
이지희 <chaehee13@naver.com>
2025-05-05 03:57:54 +0900

    [S12P31A607-141] be/comment: update logging

--- backend/api/src/main/java/com/haruspeak/api/user/presentation/UserController.java
+++ backend/api/src/main/java/com/haruspeak/api/user/presentation/UserController.java
@@ -1,8 +1,8 @@
 package com.haruspeak.api.user.presentation;
 
 import com.haruspeak.api.common.security.AuthenticatedUser;
+import com.haruspeak.api.common.security.CustomUserPrincipal;
 import com.haruspeak.api.user.application.UserService;
-import com.haruspeak.api.user.dto.CustomUserPrincipal;
 import com.haruspeak.api.user.dto.UserTag;
 import com.haruspeak.api.user.dto.response.LoginUserResponse;
 import com.haruspeak.api.user.dto.response.UserTagResponse;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 // 테스트 코드 미작성
@@ -43,8 +42,8 @@ public class UserController {
             description = "인증(로그인)된 사용자의 userId, name을 반환합니다."
     )
     public ResponseEntity<LoginUserResponse> getCurrentUser(@AuthenticationPrincipal CustomUserPrincipal user) {
-        log.info("[GET] api/user/me 로그인 사용자 정보 요청");
-        return ResponseEntity.ok(new LoginUserResponse(user.getUserId(), user.getName()));
+        log.info("[GET] api/user/me 로그인 사용자 정보 요청 user={}", user);
+        return ResponseEntity.ok(new LoginUserResponse(user.userId(), user.name()));
     }
 
     /**
