356ca11b91caab3ea190173bf0163302cfd1ed7c
원석규 <angenehms@naver.com>
2025-05-06 15:26:16 +0900

    [S12P31A607-164] ai/feat: integrate moment tag api #done

--- ai/app/utils/tag_utils.py
+++ ai/app/utils/tag_utils.py
@@ -12,30 +12,32 @@ OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 
 # 응답 스키마 정의
 class KeywordOutput(BaseModel):
-    first: str = Field(description="첫 번째 태그")
-    second: str = Field(description="두 번째 태그")
-    third: str = Field(description="세 번째 태그")
+    recommendTags: list[str] = Field(description="추천 키워드 리스트")
 
 
 parser = PydanticOutputParser(pydantic_object=KeywordOutput)
 
 # 프롬프트 템플릿
 prompt = PromptTemplate(
-    template="""
-다음은 사용자의 일기입니다. 전체 내용을 대표할 수 있는 짧고 압축적인 핵심 키워드를 추출해 주세요.
-- 각 키워드는 명사형으로 5자 이내로 작성해 주세요.
-- 감정, 주제, 활동 등을 대표하는 단어여야 합니다.
-
-[일기]
-{diary}
-
-[출력 형식]
-{format_instructions}
-""",
-    input_variables=["diary"],
+    input_variables=["content", "guide"],
     partial_variables={
         "format_instructions": parser.get_format_instructions()
     },
+    template="""
+다음은 사용자의 일기입니다. 전체 내용을 대표할 수 있는 짧고 압축적인 핵심 키워드를 추출해 주세요.
+- 각 키워드는 명사형으로 5자 이내로 작성해 주세요.
+- 각 키워드에 띄어쓰기는 생략해주세요.
+- 예를 들어 추천 키워드가 만약 "바쁜 일상", "프로젝트 회의" 라면 "바쁜일상", "프로젝트회의" 로 해주되,
+"프로젝트회의"는 6글자니까 다른 키워드를 추천해주세요.
+- 감정, 주제, 활동 등을 대표하는 단어여야 합니다.
+{guide}
+
+[일기]
+{content}
+
+[출력 형식]
+{format_instructions}
+"""
 )
 
 # LangChain 모델
@@ -43,11 +45,27 @@ llm = ChatOpenAI(
         model="gpt-4",
         temperature=0.5,
         openai_api_key=OPEN_AI_API_KEY
-    )
+)
 
 
 # 체인 만들기
-def oa_generate_tag(diary: str) -> KeywordOutput:
-    _input = prompt.format_prompt(diary=diary)
-    output = llm(_input.to_messages())
+def oa_generate_tag(
+    tags: list[str],
+    content: str
+) -> KeywordOutput:
+
+    tag_len = len(tags)
+
+    if tag_len >= 3:
+        return KeywordOutput(recommendTags="")
+
+    count = 3 - tag_len
+    guide_text = f"- 총 {count}개의 키워드를 추출해 주세요."
+
+    _input = prompt.format(
+        content=content,
+        guide=guide_text
+    )
+    output = llm.invoke(_input)
+
     return parser.parse(output.content)
