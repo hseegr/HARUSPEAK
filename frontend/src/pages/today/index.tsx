14113b395af198476e971acdcfc7759a987737fe
minjee kim <hbmj1017@naver.com>
2025-05-07 10:48:46 +0900

    [S12P31A607-170] fe/feat: create api at today page
    태그 생성 부분은 아직 back api 없어서 생긴 이후에 문제 상황 테스트 필요

--- frontend/src/pages/today/index.tsx
+++ frontend/src/pages/today/index.tsx
@@ -1,12 +1,18 @@
 import MomentCard from '@/components/MomentCard';
 import { useToday } from '@/hooks/useTodayQuery';
 import { formatDate } from '@/lib/timeUtils';
+import { MomentContent } from '@/types/common';
 
 const TodayPage = () => {
   const { data, isLoading } = useToday();
 
   if (isLoading) return <div>로딩중...</div>;
-  if (!data) return <div>오늘의 기록이 없어요. 기록을 만들어 주세요.</div>;
+  if (!data?.data || data.dataCount === 0)
+    return (
+      <div>
+        <span>오늘의 기록이 없어요. 기록을 만들어 주세요.</span>
+      </div>
+    );
 
   const firstMoment = data.data[0];
   const formattedDate = firstMoment.createdAt
@@ -14,10 +20,10 @@ const TodayPage = () => {
     : '';
 
   return (
-    <div className='flex w-full flex-col'>
+    <div className='flex flex-col w-full'>
       <div className='mb-4 text-end'>{formattedDate}</div>
-      <div className='flex w-full flex-col gap-5'>
-        {data.data.map((moment, index) => (
+      <div className='flex flex-col w-full gap-5'>
+        {data.data.map((moment: MomentContent, index: number) => (
           <MomentCard
             key={moment.momentTime || `moment-${index}`}
             moment={moment}
