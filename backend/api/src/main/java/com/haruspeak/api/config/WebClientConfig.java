6eec197d53f8381d9e8909166b096ff4fcc52804
원석규 <angenehms@naver.com>
2025-05-07 09:51:31 +0900

    [S12P31A607-174] be/feat: solve thumbnail image size limit error #done

--- backend/api/src/main/java/com/haruspeak/api/config/WebClientConfig.java
+++ backend/api/src/main/java/com/haruspeak/api/config/WebClientConfig.java
@@ -11,6 +11,7 @@ public class WebClientConfig {
     public WebClient fastApiWebClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8000")  // FastAPI 주소 (도커 포트면 그 주소)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB
                 .build();
     }
 }
