5ba1b24280c5b19dee30fa6ff84ef14c48eeb34e
minjee kim <hbmj1017@naver.com>
2025-05-08 11:05:00 +0900

    [S12P31A607-195] fe/style: prohibit drag event execpt input and textarea

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jraist7fb3o"
+    "revision": "0.7v3avbc3kb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
