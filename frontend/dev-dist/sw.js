0644b6a6077c9c6ecd8441ebbc7486b8d09a6f6c
김희수 <hseegr@naver.com>
2025-05-08 12:11:09 +0900

    [S12P31A607-212] fe/feat: create icon and favicon

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ua8if3a0ee8"
+    "revision": "0.kg1gvljpl28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
