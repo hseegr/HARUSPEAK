841e3852c7853f903b167566041d144b01ee252c
minjee kim <hbmj1017@naver.com>
2025-05-09 11:30:56 +0900

    [S12P31A607-236] fe/refactor: Separate userInfo from App.tsx to BaseLayout.tsx and Add SEO element at index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import { useEffect } from 'react';
-
 import { RouterProvider } from 'react-router-dom';
 
-import { useUserInfoQuery } from '@/hooks/useLoginQuery';
 import LoginQueryProvider from '@/providers/LoginQueryProvider';
 import { router } from '@/router';
-import useAuthStore from '@/store/userStore';
-
-const AppContent = () => {
-  const { setUser, clearUser } = useAuthStore();
-  const { data, isLoading, isError } = useUserInfoQuery();
-
-  useEffect(() => {
-    if (data) {
-      setUser(data);
-    } else if (isError) {
-      clearUser();
-    }
-  }, [data, isError, setUser, clearUser]);
-
-  if (isLoading) {
-    return <div>로딩 중...</div>; // 필요시 스플래시 컴포넌트로 교체 가능
-  }
-
-  return <RouterProvider router={router} />;
-};
 
 const App = () => {
   return (
     <LoginQueryProvider>
-      <AppContent />
+      <RouterProvider router={router} />
     </LoginQueryProvider>
   );
 };
