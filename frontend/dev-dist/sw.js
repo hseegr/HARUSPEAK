b0d2284306f1b40c0e514eb754700f5f2c223dcb
김희수 <hseegr@naver.com>
2025-05-09 13:46:40 +0900

    [S12P31A607-238] fe/fix: modify voicetotext #in-progress

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b6scc3qj77o"
+    "revision": "0.822p7li5tto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
