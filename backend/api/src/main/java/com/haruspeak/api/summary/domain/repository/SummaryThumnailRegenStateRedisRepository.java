19d247b1cc21fd849721c15bfb73c6eb303fd8d7
이지희 <chaehee13@naver.com>
2025-05-11 20:17:48 +0900

    [S12P31A607-262] be/fix: try generic type redis template

--- backend/api/src/main/java/com/haruspeak/api/summary/domain/repository/SummaryThumnailRegenStateRedisRepository.java
+++ backend/api/src/main/java/com/haruspeak/api/summary/domain/repository/SummaryThumnailRegenStateRedisRepository.java
@@ -3,8 +3,6 @@ package com.haruspeak.api.summary.domain.repository;
 import com.haruspeak.api.summary.domain.SummaryThumnailRegenState;
 
 public interface SummaryThumnailRegenStateRedisRepository {
-    void save(String userId, String summaryId, SummaryThumnailRegenState state);
-    void delete(String userId, String summaryId);
     SummaryThumnailRegenState findBySummaryId(int userId, int summaryId);
     boolean isGenereatingOfSummary(int userId, int summaryId);
 }
