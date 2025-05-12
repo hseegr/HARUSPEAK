5e97fd3d696973856305d36595cf0439c5c8aefe
minjee kim <hbmj1017@naver.com>
2025-05-12 13:41:54 +0900

    [S12P31A607-186] fe/fix: Delete console.log

--- frontend/src/providers/LoginQueryProvider.tsx
+++ frontend/src/providers/LoginQueryProvider.tsx
@@ -5,22 +5,11 @@ import useAuthStore from '@/store/userStore';
 
 const LoginProvider = ({ children }: PropsWithChildren) => {
   const { setUser } = useAuthStore();
-  const isLoginPage = window.location.pathname === '/login';
 
-  const { data, isLoading } = useUserInfoQuery();
-
-  console.log(
-    'LoginProvider - isLoginPage:',
-    isLoginPage,
-    'isLoading:',
-    isLoading,
-    'data:',
-    data,
-  );
+  const { data } = useUserInfoQuery();
 
   useEffect(() => {
     if (data) {
-      console.log('LoginProvider - 사용자 정보 설정:', data);
       setUser(data);
     }
   }, [data, setUser]);
