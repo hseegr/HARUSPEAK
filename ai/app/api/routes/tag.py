fc138fb21e520eaec32eef048096b4267c82c3fa
원석규 <angenehms@naver.com>
2025-05-13 15:43:27 +0900

    [S12P31A607-316] ai/feat: Add response code for issue when no meaning content input #done

--- ai/app/api/routes/tag.py
+++ ai/app/api/routes/tag.py
@@ -17,11 +17,19 @@ async def generate_tag(request: TagRequest):
     # 전체 request에서 필요한 데이터 꺼내 사용
     tags = request.tags
     content = request.content
+
+    if not content.strip():
+        return {"recommendTags": []}
+
     try:
+
         tag = await oa_generate_tag(
             tags,
             content
         )
+        if '아무말' in tag.recommendTags and len(tag.recommendTags) > 1:
+            tag.recommendTags.remove('아무말')
+
         return tag
     except HTTPException as e:
         raise e
