f92af97894d469c417dcf0f838f04980aabefe27
minjee kim <hbmj1017@naver.com>
2025-05-14 10:16:49 +0900

    [S12P31A607-318] fe/fix: Refactor userInfo state and call location

--- frontend/src/store/userStore.ts
+++ frontend/src/store/userStore.ts
@@ -4,7 +4,7 @@ import { AuthState, UserInfo } from '@/types/user';
 
 // 로그인 상태 관리
 const useAuthStore = create<AuthState>(set => ({
-  user: null,
+  user: undefined, // undefined: 초기 로딩 상태, null: 로그인되지 않음
   setUser: (user: UserInfo | null) => set({ user }),
   clearUser: () => set({ user: null }),
 }));
