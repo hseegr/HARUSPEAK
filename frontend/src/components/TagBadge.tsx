0c3c4a19d64d828aed121473e9338074af49b503
minjee kim <hbmj1017@naver.com>
2025-05-15 14:04:32 +0900

    [S12P31A607-320] fe/style: Enhance MomentCard and TagBadge UI

--- frontend/src/components/TagBadge.tsx
+++ frontend/src/components/TagBadge.tsx
@@ -1,5 +1,9 @@
 const TagBadge = ({ tag }: { tag: string }) => {
-  return <div className='rounded-full bg-white px-2 py-1'># {tag}</div>;
+  return (
+    <div className='rounded-full bg-white px-2 py-1 font-leeseyoon text-sm'>
+      # {tag}
+    </div>
+  );
 };
 
 export default TagBadge;
