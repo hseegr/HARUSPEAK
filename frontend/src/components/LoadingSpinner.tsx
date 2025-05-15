c9fdeddb383e822550ca51b233c28cf0fb9a5b36
minjee kim <hbmj1017@naver.com>
2025-05-15 15:45:10 +0900

    [S12P31A607-320] fe/prettier: set prettier at LoadingSpinner

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
     <div className='flex min-h-[100vh] w-full items-center justify-center bg-gray-100'>
-      <div className='flex flex-col items-center justify-center w-full gap-4 bg-white min-h-lvh max-w-96'>
+      <div className='flex min-h-lvh w-full max-w-96 flex-col items-center justify-center gap-4 bg-white'>
         <style>
           {`
             @keyframes wave {
@@ -17,11 +17,11 @@ const LoadingSpinner = () => {
             }
           `}
         </style>
-        <div className='flex items-center h-20 gap-1'>
+        <div className='flex items-center gap-1'>
           {[...Array(5)].map((_, index) => (
             <div
               key={index}
-              className='w-2 h-16 rounded-full bg-haru-green'
+              className='h-16 w-2 rounded-full bg-haru-green'
               style={{
                 animation: `wave 1.5s cubic-bezier(0.4, 0, 0.2, 1) infinite`,
                 transformOrigin: 'center',
@@ -30,7 +30,7 @@ const LoadingSpinner = () => {
             />
           ))}
         </div>
-        <p className='text-xl font-bold transition-all duration-300 text-haru-light-green/80 hover:scale-105'>
+        <p className='text-xl font-bold text-haru-light-green/80 transition-all duration-300 hover:scale-105'>
           로딩중
         </p>
       </div>
