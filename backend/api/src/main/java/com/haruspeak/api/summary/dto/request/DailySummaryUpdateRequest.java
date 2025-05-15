eefc1ab8c7b0144c848411be4c852c76f9b3c9b8
조강민 <jkm96529@naver.com>
2025-05-15 11:16:43 +0900

    [S12P31A607-330] be/feat: Add title length validation error message #done

--- backend/api/src/main/java/com/haruspeak/api/summary/dto/request/DailySummaryUpdateRequest.java
+++ backend/api/src/main/java/com/haruspeak/api/summary/dto/request/DailySummaryUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.haruspeak.api.summary.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "하루 일기 요약 수정 요청")
 public record DailySummaryUpdateRequest(
         @Schema(description = "하루 일기 제목")
+        @Size(max=50, message = "제목은 50자 이하여야 합니다.")
         String title,
         @Schema(description = "하루 일기 요약 내용")
         String content
