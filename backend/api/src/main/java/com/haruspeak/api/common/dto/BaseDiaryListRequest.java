db33493694faaaee832a6b856b66d6a362012102
이지희 <chaehee13@naver.com>
2025-05-19 18:25:42 +0900

    [S12P31A607-383] be/feat: change date type

--- backend/api/src/main/java/com/haruspeak/api/common/dto/BaseDiaryListRequest.java
+++ backend/api/src/main/java/com/haruspeak/api/common/dto/BaseDiaryListRequest.java
@@ -4,13 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -18,16 +15,21 @@ import java.time.LocalDateTime;
 public abstract class BaseDiaryListRequest {
 
     @Schema(description = "이전 목록 조회 시 받은 커서값", nullable = true, example = "2025-05-01T15:00:00")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    LocalDateTime before;
+    String before;
 
     @Schema(description = "조회 시작일", nullable = true, example = "2025-05-01")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    protected LocalDate startDate;
+    @Pattern(
+            regexp = "^\\d{4}-\\d{2}-\\d{2}",
+            message = "잘못된 날짜 형식입니다. (예: 2025-05-19)"
+    )
+    protected String startDate;
 
     @Schema(description = "조회 종료일", nullable = true, example = "2025-05-31")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    protected LocalDate endDate;
+    @Pattern(
+            regexp = "^\\d{4}-\\d{2}-\\d{2}",
+            message = "잘못된 날짜 형식입니다. (예: 2025-05-19)"
+    )
+    protected String endDate;
 
     @Schema(description = "조회 개수 (1~30)", example = "30")
     @NotNull(message = "조회 개수가 널이어서는 안됩니다.")
