f209fc3f86bd3b9d8eb22d5c35cb4c15a0404e99
minjee kim <hbmj1017@naver.com>
2025-05-19 14:02:57 +0900

    [S12P31A607-372] fe/fix: Modify Loading Spinner

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2s0fmif18eg"
+    "revision": "0.a8bl0kh548g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
