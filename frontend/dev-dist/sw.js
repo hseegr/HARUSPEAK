0895f510e9aea1fb699427b4fe2d1919a9470636
minjee kim <hbmj1017@naver.com>
2025-05-19 13:46:56 +0900

    [S12P31A607-372] fe/fix: Modify width and hidden scroll

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i5rb05093e"
+    "revision": "0.2s0fmif18eg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
